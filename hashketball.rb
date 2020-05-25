0eb2af09d44e63e74ef37f24f1a972bf3ba4e437
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-25 21:03:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -197,23 +197,6 @@ end
 
 
 
-def player_stats(input)
-  game_hash.each do |team, team_info|
-    team_info.each do |key, value|
-      if key == :players
-        value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # having player name inside the hash was a bad idea!
-            return player
-          end
-        end
-      end
-    end
-  end
-end
-
-
-
 
 def big_shoe_rebounds
   big_shoe = 0
